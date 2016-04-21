55434a66d281f71c15a6b876dbb6cd471835e937
Ilya Puzanov <Ilya Puzanov>
2016-04-22 02:29:02 +0300

    initial

---
+++ desktop/src/ua/com/zoi/gdx/bb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ua.com.zoi.gdx.bb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ua.com.zoi.gdx.bb.BludBourne;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BludBourne(), config);
+	}
+}
